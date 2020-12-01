3aa4fad226d0508c351a4d063f64c240b7ce4f3d
Ali Ok <aliok@redhat.com>
2020-12-01 14:24:57 +0300

    Created project

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Ali Ok (ali.ok@apache.org)
+ * 01/12/2020 14:18
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
